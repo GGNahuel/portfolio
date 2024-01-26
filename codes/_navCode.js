42d4e4be74537f4babe3db71016d85491547a015
Nahuel Gomez Gahn <nahuelg8799@gmai.com>
2024-01-26 10:51:33 -0300

    Fixed ring transition with items

--- codes/_navCode.js
+++ codes/_navCode.js
@@ -1,6 +1,5 @@
-const $ring = document.querySelector("#mainHeader>nav")
+const $ring = document.querySelector("#mainHeader")
 const $navItems_Container = $ring.querySelectorAll(".navItem_Container")
-// const $navItems = $ring.querySelectorAll(".navItem")
 
 const $KEYFRAME_ring = [{
     transform: window.innerWidth > 719 ? "translateX(calc((100vw / -2) - 25%))" : "translateY(-100vh)",
@@ -8,16 +7,18 @@ const $KEYFRAME_ring = [{
 }]
 const $KEYFRAME_ringItems = (angle) => [
     { transform: "rotate(270deg)", offset: 0.35 },
-    { transform: "rotate(360deg)", offset: 0.75 },
     { transform: `rotate(${angle}deg)`, offset: 1 }
 ]
-const _ringAnimationProperties = { duration: 1500, delay: 1000,easing: "ease", fill: "forwards" }
+const _ringAnimationProperties = { duration: 1500, delay: 1500,easing: "ease", fill: "forwards" }
+
+const distribuirItemsEnAnillo = (anguloInicial, anguloRelativoFinal, multiplicador) => 
+    (anguloInicial + multiplicador * (anguloRelativoFinal / ($navItems_Container.length -1)))
 
 if (window.innerWidth > window.innerHeight) {
     $ring.style.height = "calc(100vh - 1rem)"
     $navItems_Container.forEach((element, index) => {
-        element.style.transform = `rotate(${180 + 15 * index}deg)`
-        element.animate($KEYFRAME_ringItems(450 + index * (180 / ($navItems_Container.length + 2))), _ringAnimationProperties)
+        element.style.transform = `rotate(${distribuirItemsEnAnillo(135, 90, index)}deg)`
+        element.animate($KEYFRAME_ringItems(distribuirItemsEnAnillo(405, 135, index)), _ringAnimationProperties)
     })
 }
 else {
@@ -25,3 +26,4 @@ else {
 }
 
 $ring.animate($KEYFRAME_ring, _ringAnimationProperties)
+    .finished.then(() => { console.log("overflow del root scroll") })
