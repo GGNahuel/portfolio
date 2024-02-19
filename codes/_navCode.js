3243095c745907e85f19de5545e8f5f26dc46ae9
Nahuel Gomez Gahn <nahuelg8799@gmai.com>
2024-02-19 00:09:03 -0300

    better organization, navscript changes & presentation section

--- codes/_navCode.js
+++ codes/_navCode.js
@@ -1,36 +1,110 @@
-const $root = document.querySelector("#root")
-const $ring = document.querySelector("#mainHeader")
-const $navItems_Container = $ring.querySelectorAll(".navItem_Container")
-
-const widthInScreen_$ring = window.innerWidth / 2
+const finalRingPosition = window.innerWidth > 719 ? "calc((100vw / -2) - 25%)" : "0 -100vh"
 
 const $KEYFRAME_ring = [{
-    transform: window.innerWidth > 719 ? "translateX(calc((100vw / -2) - 25%))" : "translateY(-100vh)",
+    translate: finalRingPosition,
     offset: 1
 }]
 const $KEYFRAME_ringItems = (angle) => [
-    { transform: "rotate(270deg)", offset: 0.35 },
-    { transform: `rotate(${angle}deg)`, offset: 1 }
+    { rotate: "270deg", offset: 0.35 },
+    { rotate: `${angle}deg`, offset: 1 }
 ]
-const _ringAnimationProperties = { duration: 1500, delay: 1500,easing: "ease", fill: "forwards" }
+const _ringAnimationProperties = { duration: 1500, delay: 1500,easing: "ease"}
 
-const distribuirItemsEnAnillo = (anguloInicial, anguloRelativoFinal, multiplicador) => 
-    (anguloInicial + multiplicador * (anguloRelativoFinal / ($navItems_Container.length -1)))
-
-if (window.innerWidth > window.innerHeight) {
-    $ring.style.height = "calc(100vh - 1rem)"
-    $navItems_Container.forEach((element, index) => {
-        element.style.transform = `rotate(${distribuirItemsEnAnillo(135, 90, index)}deg)`
-        element.animate($KEYFRAME_ringItems(distribuirItemsEnAnillo(405, 135, index)), _ringAnimationProperties)
-    })
-}
-else {
-    $ring.style.width = "calc(100vw - 5rem)"
+function calcularAnguloSegunItem (anguloInicial, anguloRelativoFinal, multiplicador) {
+    return (
+        anguloInicial + multiplicador * (anguloRelativoFinal / ($navItems_Container.length -1))
+    )
 }
 
-$ring.animate($KEYFRAME_ring, _ringAnimationProperties)
+function definirStylesSegunVW () {
+    if (window.innerWidth > 719) {
+        $ring.style.width = "calc(100vh - 1rem)"
+        $ring.style.top = "0.5rem"
+    
+        const ringWidth = $ring.offsetWidth
+        $main.style.marginLeft = `${ringWidth * 0.25}px`
+        $main.style.width = `calc(100vw - ${ringWidth * 0.25}px - ${ancho$scrollbar})`
+    }
+    else {
+        $ring.style.width = "calc(100vw - 2rem)"
+        // $ring.style.left = "1rem"
+        $main.style.marginLeft = "0"
+        $main.style.width = "100%"
+    }
+}
+
+const defaultAngles = []
+let startAnimationFinished = false
+
+window.addEventListener("load", () => {
+    let anguloDistribucionItems = window.innerWidth > 719 ? 135 : 0
+
+    definirStylesSegunVW()
+
+    $ring.animate($KEYFRAME_ring, _ringAnimationProperties)
     .finished.then(() => {
-        const $main = document.querySelector("main")
-        $root.style.overflow = "visible"
-        $main.style.display = "block"
+        $ring.style.translate = finalRingPosition
+        $navItems_Container[0].classList.add("current")
+        $root.style.overflowY = "visible"
+        $main.style.display = "flex"
+        // console.log("animcaion terminada")
+        startAnimationFinished = true
+        ajustarAncho()
     })
+
+    $navItems_Container.forEach(($element, index) => {
+        $element.style.rotate = `${calcularAnguloSegunItem(135, 90, index)}deg`
+        
+        const anguloFinal = calcularAnguloSegunItem(405, anguloDistribucionItems, index)
+        $element.animate($KEYFRAME_ringItems(anguloFinal), _ringAnimationProperties).finished.then(() => {
+            $element.style.rotate = `${anguloFinal}deg`
+            defaultAngles.push(calcularAnguloSegunItem(405, 135, index))
+        })
+    })
+})
+window.addEventListener("resize", () => {
+    const newFinalRingPosition = window.innerWidth > 719 ? "calc((100vw / -2) - 25%)" : "0 -100vh"
+    if (startAnimationFinished) {
+        $ring.style.translate = newFinalRingPosition
+        let anguloDistribucionItems = window.innerWidth > 719 ? 135 : 0
+        $navItems_Container.forEach(($element, index) => {
+            $element.style.rotate = `${calcularAnguloSegunItem(405, anguloDistribucionItems, index)}deg`
+        })
+    }
+    definirStylesSegunVW()
+})
+
+
+let pixelsForScroll = 0
+let lastScrollTopPosition = 0
+$root.addEventListener("scroll", () => {
+    pixelsForScroll = $root.scrollTop - lastScrollTopPosition
+    lastScrollTopPosition = $root.scrollTop
+
+    const ringRadius = $ring.offsetHeight / 2
+    const newAngle = truncWithDecimals(((pixelsForScroll / ringRadius) * (180 / Math.PI)), 3) * 90 / 360 * -1
+    
+    $navItems_Container.forEach(($navItem, index) => {
+        const actualAngle = Number (window.getComputedStyle($navItem).rotate.match(/(\d*\.?\d{0,3})/)[0])
+        $navItem.style.rotate = `${lastScrollTopPosition !== 0 ? actualAngle + newAngle : defaultAngles[index]}deg`
+
+        $navItem.classList.toggle("current", false)
+    })
+    // let sectionInScreenIndex = 0
+    for (let index = 0; index < $mainSections.length; index++) {
+        const $section = $mainSections[index]
+        const eqSectionProps = {
+            posYstart: $section.offsetTop,
+            posYfinish: $section.offsetTop + $section.offsetHeight,
+            posYhalf: $section.offsetTop + $section.offsetHeight / 2
+        }
+        let navItem_index = index
+        if (!(lastScrollTopPosition >= eqSectionProps.posYstart && lastScrollTopPosition < eqSectionProps.posYfinish)) {
+            continue
+        }
+        if (lastScrollTopPosition >= eqSectionProps.posYhalf && index < $navItems_Container.length - 1) {
+            navItem_index ++
+        }
+        $navItems_Container[navItem_index].classList.toggle("current", true)
+    }
+})
--- codes/mainCode.js
+++ codes/mainCode.js
@@ -1,1 +1,19 @@
+const $root = document.querySelector("#root")
+const $ring = document.querySelector("#mainHeader")
+const $navItems_Container = $ring.querySelectorAll(".navItem_Container")
 
+const $main = document.querySelector("main")
+const $mainSections = document.querySelectorAll("main > section")
+
+
+const ancho$scrollbar = '17px'
+function ajustarAncho() {
+    var tieneScroll = $root.scrollHeight > window.innerHeight
+    $root.style.paddingRight = tieneScroll ? '0' : ancho$scrollbar
+}
+window.addEventListener('load', ajustarAncho)
+window.addEventListener('resize', ajustarAncho)
+
+function truncWithDecimals(number = Number, decimals = 0){
+    return Math.trunc(number * Math.pow(10, decimals)) / Math.pow(10, decimals)
+}
