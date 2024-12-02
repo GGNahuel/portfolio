81b120748895a0a206b50d206c853d21eaacf190
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-12-02 12:52:17 -0300

    translation of index page

---
+++ codes/translation/indexPageTranslation.js
@@ -0,0 +1,159 @@
+const translation = {
+  spanish: {
+    navbar: ["Presentación", "Habilidades", "Proyectos", "Estudios", "Contacto"],
+    title: "Lo que necesitas para que tu proyecto <span class='important'>llegue alto</span>",
+    buttons: {
+      resume: "Curriculum",
+      toContact: "Hablemos de lo que necesita"
+    },
+    presentationSection: {
+      profileCard: {
+        role: "Desarrollador web FullStack",
+        text: "Dispuesto a dar el 101% de mis capacidades en lo que hago. Siempre atento a ese extra que pueda aportar a hacer un trabajo de muy buena calidad."
+      },
+      features: ["Cooperación", "Prolijidad", "Desarrollo de base de datos", "Seguridad web", "Diseño para múltiples dispositivos", "UX/UI"],
+      aboutMe: {
+        title: "¡Hola!. Aquí podrás conocer más de mí.",
+        formattedText: `<p>Actualmente tengo 25 años y vivo en Argentina, más específicamente en la provincia de Santa Fe, lugar donde nací.</p>
+          <br><p>Disfruto de actividades como leer, disfrutar del aire libre y salidas con amigos. Mis hobbies son el tiro con arco y la fotografía.
+            Actividades a las que me gusta ponerle esfuerzo e ir mejorando con el tiempo.
+          </p>
+          <br><p>Mi recorrido en la programación empezó de adolescente cuando de curiosidad investigué sobre el proceso de creación de videojuegos. 
+            Más tarde opté por estudiar más al respecto, y, al encontrar gusto y pasión en esto, decidí empezar a formarme profesionalmente en esta área.
+          </p>
+          <p>Personalmente me gusta este ámbito debido a que siempre hay espacio para ir mejorando las habilidades o conocimientos que uno ya tiene.
+            Me gusta enfrentarme a los distintos retos que surgen y buscar la mejor solución posible para que algo funcione como se espera.
+        </p>`
+      }
+    },
+    skillsSection: {
+      title: "Resumen de habilidades técnicas",
+      categories: ["Lenguajes de programación", "Frameworks de desarrollo y extensiones",
+        "Herramientas de marcado, estilos y plantillas", "Bases de datos", "Otras herramientas de desarrollo"
+      ]
+    },
+    projectsSection: {
+      title: "Proyectos"
+    },
+    studiesSection: {
+      title: "Formación",
+      unl: `Febrero 2019 - diciembre 2021, <strong>Tecnicatura</strong> en informática de gestión en la Facultad de Ingenierías y Ciencias Hídricas de la Universidad
+        Nacional del litoral. Estudios incompletos`,
+      firstStepsFront: `Enero - marzo 2023, curso "Primeros pasos del desarrollo frontEnd" por Argentina programa 4.0`,
+      fullStackBootcamp: `Abril 2023 - julio 2024, bootcamp para <strong>desarrollador fullstack</strong> por parte de EggCooperation</p>
+        <p class="comment">En este bootcamp además de aprender y formarme sobre programación, también tuve la oportunidad
+          de desarrollar e ir mejorando <strong>habilidades blandas</strong> a traves del trabajo en equipo y la cooperación que en el mismo se requiere.
+          Por ejemplo: el explicar o preguntar en cuánto contenido; la toma de decisiones; y el presentar, escuchar y poner en 
+          común ideas o posibles soluciones a los distintos retos que se nos iban presentando. 
+        </p>
+        <p class="comment">La modalidad de este cursado fue a traves de clases en vivo y trabajos grupales. El cuál aprobé con un
+          trabajo final en un grupo de 4 integrantes en el que nos tocó desarrollar una web completa.
+        </p>
+        <p class="comment">Este trabajo se desarrolló usando <strong>metodologías ágiles</strong>, y si bien estuve principalmente como desarrollador backend, también 
+          me tocó desempeñar el rol de <strong>líder técnico</strong>. Administrando el repositorio remoto de github y manejando cuestiones de git.
+      `,
+      codigoFacilitoCourses: `Mayo 2024, distintas capacitaciones y cursos de <a href="https://codigofacilito.com/">Código facilito</a>.</p>
+        <ul>
+          <li>Curso de fundamentos de <strong>arquitectura de software</strong></li>
+          <li>Capacitación sobre testing en Java</li>
+          <li>Curso de Git</li>
+          <li>Curso de Github</li>
+          <li>Curso Profesional de deploy en servidores</li>
+        </ul>`,
+      others: `Además de lo mencionado anteriormente quisiera no dejar de lado que también profundicé y amplié mis conocimientos de forma autodidacta.
+        A traves de documentaciones oficiales, foros, medios audiovisuales, y personas de las que tuve la oportunidad de comunicarme y aprender de ellas.`
+    },
+    contactSection: {
+      title: "Formulario de contacto",
+      subtitle: "Responda las preguntas para saber más de lo que necesita, o bien arme su propio mensaje",
+      formLabels: {
+        name: "Ingrese su nombre",
+        email: "Ingrese su email",
+        about: "¿A qué se dedica usted o la empresa a la que pertenece?",
+        service: "¿Qué tipo de proyecto, servicio o personal está buscando?",
+        expected: "¿Qué es lo que espera de mí al trabajar con usted/es?",
+        message: "Mensaje, comentarios extra"
+      },
+      button: "Contactarse",
+      alsoMessage: `También puede buscarme en <a href="https://www.linkedin.com/in/nahuel-gomez-gahn/" target="_blank">Linkedin</a> o escribiéndome al e-mail nahuelg8799@gmail.com`
+    }
+  },
+
+  english: {
+    navbar: ["About Me", "Skills", "Projects", "Studies", "Contact me"],
+    title: "What you need to make your projects <span class='important'>grow</span>",
+    buttons: {
+      resume: "Resume",
+      toContact: "Let's talk about what you need"
+    },
+    profileSection: {
+      profileCard: {
+        role: "FullStack web Developer",
+        text: "Ready to give the 101% to the work. Always looking for that 1% extra that can help to make the projects to reach the best quality."
+      },
+      features: ["Cooperative", "Attention to details", "Database development", "Web security", "Multi-platform design", "UX/UI"],
+      aboutMe: {
+        title: "Hello!. Here you can know more about me.",
+        formattedText: `<p>I'm 25 years old and live in Argentina at the moment, to be more specific in the province of Santa Fe, place where I was born.</p>
+            <br><p>I enjoy activities like reading book, spending time outdoors and hanging out with my friends. My hobbies are archery and photography.
+              Activities that I like to dedicate time and make improvements.
+            </p>
+            <br><p>My journey in programming begun when I was a teenager and wanted to know more about the process of making video-games. 
+              Later I choose to study more of this area, and by finding in it enjoyment and passion, I decided start training myself as a developer.
+            </p>
+            <p>I like this activity because there is always room to improve the skills or knowledge someone already have. 
+              I feel motivated when I try to find the best possible solution to the different upcoming challenges.
+        </p>`
+      }
+    },
+    skillsSection: {
+      title: "Technical skills",
+      categories: ["Programming languages", "Frameworks and extensions", "Markup tools, styles and templates",
+        "Databases", "Other development tools"
+      ]
+    },
+    projectsSection: {
+      title: "Projects"
+    },
+    studiesSection: {
+      title: "Studies",
+      unl: `February 2019 - December 2021. <strong>Technician</strong> in computer sciences with a management focus, Universidad Nacional del Litoral (UNL).
+        Incomplete.`,
+      firstStepsFront: `January - March 2023. "First steps in frontend development, Argentina Programa 4.0`,
+      fullStackBootcamp: `March 2023 - June 2024. <strong>Fullstack web developer</strong> bootcamp, Egg Cooperation</p>
+        <p class="comment">In this bootcamp, in addition to learning about programming technologies, I had the opportunity
+        to develop and improve my <strong>soft skills</strong> by teamwork and cooperation. For example: 
+        explain or ask about the content of something related; the decision-making process; and give, listen and pool ideas
+        or possible solution to the different upcoming challenges.</p>
+        <p class="comment">The work in this bootcamp was by online classes and group activities. At the end, in a group of 4 study partners, we have to 
+        develop a full-stack web application.</p>
+        <p class="comment">This project was using <strong>agile methodologies</strong>, and in this case I was not only
+        in the backend team, but also I was designed as a <strong>tech leader</strong>. Managing the remote repository in github and git issues.</p>
+        `,
+        codigoFacilitoCourses: `April - May 2024. Different courses and training from <a href="https://codigofacilito.com/">Código facilito</a>.</p>
+        <ul>
+          <li>Course about basics of <strong>software architecture</strong></li>
+          <li>Training about Java testing</li>
+          <li>Git course</li>
+          <li>GitHub course</li>
+          <li>Course about deploy</li>
+        </ul>`,
+        others: `Also, I don't want to ignore that I learn more in an autodidact form. By official documentations, forums, audiovisual media, and people that
+        I had the chance to talk and learn from them.`
+    },
+    contactSection: {
+      title: "Contact form",
+      subtitle: "Answer the questions to know more about what you need, or write your own message below",
+      formLabels: {
+        name: "Name",
+        email: "Email",
+        about: "What do you, or the company you work for, do?",
+        service: "What kind of project, service or role you are looking for?",
+        expected: "What do you expect from me while working with you?",
+        message: "Message, extra comments"
+      },
+      button: "Send",
+      alsoMessage: `Also you can contact me in <a href="https://www.linkedin.com/in/nahuel-gomez-gahn/" target="_blank">Linkedin</a> or sending an email to nahuelg8799@gmail.com`
+    }
+  }
+}
