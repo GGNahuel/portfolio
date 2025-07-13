0d6c491bfe36dc24eb345c6dd9dba5fd9a920380
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-07-13 00:14:10 -0300

    start changing styles in general

--- codes/mainSectionsCode.js
+++ codes/mainSectionsCode.js
@@ -30,7 +30,7 @@ class ProjectElement {
     const mainProjectsElements = {
       expand_button: `<a class="seeMore" href="${this.expand_link}"><svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none">
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
-      <path fill="#000000" fill-rule="evenodd" d="M10 3a7 7 0 100 14 7 7 0 000-14zm-9 7a9 9 0 1118 0 9 9 0 01-18 0zm8-4a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zm.01 8a1 1 0 102 0V9a1 1 0 10-2 0v5z"></path>
+      <path fill="#ffffffff" fill-rule="evenodd" d="M10 3a7 7 0 100 14 7 7 0 000-14zm-9 7a9 9 0 1118 0 9 9 0 01-18 0zm8-4a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zm.01 8a1 1 0 102 0V9a1 1 0 10-2 0v5z"></path>
       </g></svg></a>`,
       features_section: `<div><p>${lang == "spanish" ? "Características" : "Features"}:</p><ul class="pC_features">${featuresList}</ul></div>`,
       link_element: this.link ? `<a class="pC_link" href="${this.link}" target="_blank">${lang == "spanish" ? "Enlace al sitio" : "Link to website"}</a>` : "",
